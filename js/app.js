adae35268991d87612aa4e4acbf25ba594bb7a5c
Levie <svpv.hotro@gmail.com>
2022-08-13 09:56:56 +0700

    a

--- js/app.js
+++ js/app.js
@@ -12,6 +12,81 @@ app.controller("myController", function ($scope, $rootScope) {
     }
     return user.category != 0;
   };
+  var findIndexOfKey = function (searchKey) {
+    for (var i = 0; i < localStorage.getItem("itemsArray").length; i++) {
+      console.log(localStorage.getItem("itemsArray"));
+      var key = localStorage.key(i);
+      if (key === searchKey) return i;
+    }
+    return -1;
+  };
+  $rootScope.carItems = [];
+  var cartItems = $rootScope.carItems;
+  $rootScope.addtocart = function (id, name, price, quantity) {
+    var oldItems = JSON.parse(localStorage.getItem("itemsArray")) || [];
+    if (oldItems.length == 0 || oldItems == null) {
+      var newItem = {
+        id: id,
+        name: name,
+        price: price,
+        quantity: quantity,
+      };
+
+      oldItems.push(newItem);
+
+      localStorage.setItem("itemsArray", JSON.stringify(oldItems));
+    }
+    var count = 0;
+    for (const key in oldItems) {
+      if (oldItems[key].id == id) {
+        count++;
+        console.log(oldItems[i].name + "dsd");
+        oldItems[key].quantity = oldItems[key].quantity + 1;
+
+        // return false;
+      }
+    }
+    if (count == 0) {
+      var newItem = {
+        id: id,
+        name: name,
+        price: price,
+        quantity: quantity,
+      };
+
+      oldItems.push(newItem);
+
+      localStorage.setItem("itemsArray", JSON.stringify(oldItems));
+    }
+
+    for (const key in oldItems) {
+      if (oldItems[key].id) {
+        console.log(oldItems[key].id);
+        console.log(typeof oldItems[key]);
+        console.log("tìm thấy item trong localStorage: ");
+      }
+      // else{
+      //   console.log(' không tìm thấy item trong localStorage');
+      //   var newItem = {
+      //     id: id,
+      //     name: name,
+      //     price: price,
+      //     quantity: quantity,
+      //   };
+
+      //   oldItems.push(newItem);
+
+      //   localStorage.setItem("itemsArray", JSON.stringify(oldItems));
+      // }
+      //  console.log(oldItems[key].id.includes(id))
+    }
+
+    const includesTwenty = oldItems.includes(id);
+    if (!includesTwenty) {
+    }
+
+    console.log(oldItems); //true
+  };
 });
 app.config(function ($routeProvider) {
   $routeProvider
@@ -34,7 +109,7 @@ app.config(function ($routeProvider) {
     })
     .when("/cart", {
       title: "Cart",
-      templateUrl: "Cart.html",
+      templateUrl: "cart2.html",
     })
     .when("/contact", {
       title: "ContactUS",
@@ -45,11 +120,12 @@ app.config(function ($routeProvider) {
       redirectTo: "/",
     });
 });
+
 app.run(function ($rootScope, $location) {
-    $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
-        $rootScope.title = current.$$route.title;
-        console.log(current.$$route.title);
-    });
+  $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
+    $rootScope.title = current.$$route.title;
+    console.log(current.$$route.title);
+  });
 });
 
 // $routeProvider
@@ -87,4 +163,9 @@ app.run(function ($rootScope, $http) {
     });
   });
 });
-
+function cartItem(pcode, name, price, quantity) {
+  this.pcode = pcode;
+  this.name = name;
+  this.price = price * 1;
+  this.quantity = quantity * 1;
+}
